258dbb12ff51b5ebc5b63bc1d6d74b199732f051
fikrimilano <fikrimilano1@gmail.com>
2023-02-25 14:12:27 +0700

    Add static fragment

--- app/src/main/java/com/example/pertemuan2tugas/MainActivity.java
+++ app/src/main/java/com/example/pertemuan2tugas/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.pertemuan2tugas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManagerHelper fragmentManagerHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManagerHelper = new FragmentManagerHelper(getSupportFragmentManager(), R.id.fragment_container);
+
+        DescriptionFragment fragment = DescriptionFragment.newInstance(R.string.description_initial);
+        fragmentManagerHelper.add(fragment);
     }
 }
