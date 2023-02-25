c4ee116f7e3558b18cb0f5f5d764dfd03ae61a3a
fikrimilano <fikrimilano1@gmail.com>
2023-02-25 09:21:52 +0700

    Add dynamic fragment logic

--- app/src/main/java/com/example/pertemuan2tugas/MainActivity.java
+++ app/src/main/java/com/example/pertemuan2tugas/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.pertemuan2tugas;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManagerHelper fragmentManagerHelper;
+
+    private Button btnAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManagerHelper = new FragmentManagerHelper(getSupportFragmentManager(), R.id.fragment_container);
+
+        btnAction = findViewById(R.id.action_button);
+        btnAction.setOnClickListener(this::onActionButtonClicked);
+
+        DescriptionFragment fragment = DescriptionFragment.newInstance(R.string.description_initial);
+        fragmentManagerHelper.add(fragment);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void onActionButtonClicked(View v) {
+        DescriptionFragment currentFragment = (DescriptionFragment) fragmentManagerHelper.getCurrentFragment();
+        if (currentFragment == null) return;
+
+        int resIdDescription = currentFragment.getResIdDescription();
+        if (resIdDescription == R.string.description_initial) {
+            DescriptionFragment fragment = DescriptionFragment.newInstance(R.string.description_more);
+            fragmentManagerHelper.replace(fragment);
+            updateActionButton(R.string.text_action_button_state_more);
+        } else {
+            DescriptionFragment fragment = DescriptionFragment.newInstance(R.string.description_initial);
+            fragmentManagerHelper.replace(fragment);
+            updateActionButton(R.string.text_action_button_state_initial);
+        }
+    }
+
+    private void updateActionButton(@StringRes int resId) {
+        btnAction.setText(resId);
     }
 }
